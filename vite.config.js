43ac510c9ec7d1f822d19ff1182157a6f5c607f6
João Cumbo Mabiala <mabialajoaocumbo@gmail.com>
2025-06-13 17:34:50 +0100

    deploy
    fazer deploy

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "/to-do-list",
+  base: process.env.VITE_BASE_PATH || "/task-manager",
 });
